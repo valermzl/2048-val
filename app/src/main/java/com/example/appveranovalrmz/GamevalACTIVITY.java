7798949f945c6f57b4daae34b59ef547e02bb5cb
Cursos LABDESI <Cursos@T20205.ds.iteso.mx>
2019-07-09 14:39:24 -0500

    parte1 app

---
+++ app/src/main/java/com/example/appveranovalrmz/GamevalACTIVITY.java
@@ -0,0 +1,14 @@
+package com.example.appveranovalrmz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GamevalACTIVITY extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gameval_activity);
+    }
+}
